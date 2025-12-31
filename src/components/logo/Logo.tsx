ddc71a8eb692971c119edac53001ee70068aef5d
Ahmet Adem <ahmet.adem@technoperia.com>
2025-12-31 15:09:07 +0100

    fix:logo font

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import {Poppins} from "next/font/google";
+
 interface LogoProps {
    isLink?: boolean;
    showText?: boolean;
@@ -7,13 +9,20 @@ interface LogoProps {
    width?: number;
    height?: number;
 }
+
+const poppins = Poppins({
+   subsets: ['latin'],
+   weight: ['400', '500', '600', '700', '800', '900'],
+   variable: '--font-poppins',
+});
+
 export const Logo = ({isLink = false, showText = false, className = "", width = 80, height = 80}: LogoProps) => {
     return (
       <>
               {isLink ? <Link href="/" className={`flex items-center ${className}`}>
               <Image src="/logo.png" alt="Imampoplaka Logo" width={width} height={height} />
-              {showText && <span className="hidden lg:block whitespace-nowrap text-xl font-semibold sm:text-2xl text-foreground">
-                imam<span className="text-secondary">poplaka</span>
+              {showText && <span  className={`hidden lg:block whitespace-nowrap text-xl font-semibold sm:text-2xl text-foreground ${poppins.className}`}>
+                imam<span className={`text-secondary`}>poplaka</span>
               </span> }
             </Link> : <div className={className}><Image src="/logo.png" alt="Imampoplaka Logo" width={width} height={height} /></div>   }
       </>
